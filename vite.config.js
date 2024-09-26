b66d8552b50597518e174b0b5c1ef0bd309e26d0
Demetryj <103371743+Demetryj@users.noreply.github.com>
2024-09-26 20:39:35 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,14 +8,8 @@ export default defineConfig({
     sourcemap: true,
     base: '/calendar-test/',
   },
-  rollupOptions: {
-    output: {
-      assetFileNames: assetInfo => {
-        if (assetInfo.name?.endsWith('.css')) {
-          return './assets/[name]-[hash].[ext]'; // Измените на нужный вам путь
-        }
-        return '[name]-[hash].[ext]';
-      },
-    },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
   },
 });
